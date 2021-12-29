85011314bf32cb114d008c22ddecb17af7841edd
Mihai Mihailescu <mihai.mihailescu@stonesouptech.com>
2021-12-29 23:57:27 +0200

    some name changes

--- data/__init__.py
+++ data/__init__.py
@@ -1,1 +1,2 @@
-from data.models.user import QueryUser, User, Token
+from data.models.user import QueryUser, User, Token, Base
+from data.models import  SQLALCHEMY_DATABASE_URL_SYNC
--- data/alembic/env.py
+++ data/alembic/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from models import  SQLALCHEMY_DATABASE_URL_SYNC, Base,  ExtraBase
+from data import SQLALCHEMY_DATABASE_URL_SYNC, Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
