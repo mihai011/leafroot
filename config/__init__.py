20d50f5a186e46572fd1560b2dbc998a5bfeed07
mihai011 <mihaimihai011@gmail.com>
2022-08-27 10:43:53 +0000

    Added circle ci rabbitmq and variables in init.py

--- config/__init__.py
+++ config/__init__.py
@@ -7,32 +7,40 @@ if "ENV" not in os.environ:
     print("FATAL! Environment variable not set up! Helping values: 'prod' or 'dev'")
     sys.exit(1)
 
-if os.environ['ENV'] not in ["dev", "prod"]:
-    print("FATAL! Environment variable not set up correctly! Helping values: 'prod' or 'dev'")
+if os.environ["ENV"] not in ["dev", "prod"]:
+    print(
+        "FATAL! Environment variable not set up correctly! Helping values: 'prod' or 'dev'"
+    )
     sys.exit(1)
 
 config = {}
 # check up if environment is production
-if os.environ['ENV'] == "dev":
+if os.environ["ENV"] == "dev":
     if os.path.exists(".env"):
         config = dotenv_values(".env")
     else:
-        print("FATAL! Environment file not present! Try a '.env' in the root of the project!")
+        print(
+            "FATAL! Environment file not present! Try a '.env' in the root of the project!"
+        )
         sys.exit(1)
 
-config['ENVIRONMENT'] = os.environ['ENV']
+config["ENVIRONMENT"] = os.environ["ENV"]
 # check up if environment is production
-if os.environ['ENV'] == "prod":
+if os.environ["ENV"] == "prod":
     # retrieve secrets frome external api's here
     pass
 
-mandatory_fields = ["POSTGRES_DB",
-"POSTGRES_HOST",
-"POSTGRES_USER",
-"POSTGRES_PASSWORD",
-"ACCESS_TOKEN_EXPIRE_MINUTES",
-"SECRET_KEY",
-"ALGORITHM"]
+mandatory_fields = [
+    "POSTGRES_DB",
+    "POSTGRES_HOST",
+    "POSTGRES_USER",
+    "POSTGRES_PASSWORD",
+    "ACCESS_TOKEN_EXPIRE_MINUTES",
+    "SECRET_KEY",
+    "ALGORITHM",
+    "REDIS_HOST",
+    "RABBITMQ_HOST",
+]
 
 # check up if environment dictates something else
 # with the values for the environment
@@ -48,7 +56,9 @@ for mandatory_field in mandatory_fields:
         missing_fields.append(mandatory_field)
 
 if missing_fields:
-    missing_fields = '\n'.join(missing_fields)
-    print("FATAL! The following environment "\
-        "variables are missing:\n{}".format(missing_fields))
+    missing_fields = "\n".join(missing_fields)
+    print(
+        "FATAL! The following environment "
+        "variables are missing:\n{}".format(missing_fields)
+    )
     os._exit(1)
