828a6bd907ff7ec8d813e00893950b56cf122352
root <mihaimihai011@gmail.com>
2022-08-28 12:41:26 +0000

    Empty

--- locustfile.py
+++ locustfile.py
@@ -1,11 +1,14 @@
+"""
+Some docstring
+"""
+
 from locust import HttpUser, task
+
 
 class HelloWorldUser(HttpUser):
     @task
     def login(self):
-        self.client.post("http://localhost:8001/users/login", json={
-    "email":"a@gmail.com",
-    "username": "mika",
-    "password": "control"
-})
-        
+        self.client.post(
+            "http://localhost:8001/users/login",
+            json={"email": "a@gmail.com", "username": "mika", "password": "control"},
+        )
