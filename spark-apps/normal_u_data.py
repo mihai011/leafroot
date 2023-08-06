77e843ddc71ea2a12be22dced21baf226b27988f
Mihai Mihailescu <mihaimihailescu@Mihais-MacBook-Air.local>
2023-08-07 00:07:54 +0300

    Experimented a bit with spark

---
+++ spark-apps/normal_u_data.py
@@ -0,0 +1,40 @@
+from time import time
+import pandas as pd
+import polars as pl
+import tqdm
+
+
+with open("/opt/spark-data/ml-100k/u.data") as f:
+    data = f.readlines()
+
+data_df = {"client_id": [], "movie_id": [], "rating": [], "timestamp": []}
+for line in tqdm.tqdm(data):
+    line = line.split()
+    data_df["client_id"].append(line[0])
+    data_df["movie_id"].append(line[1])
+    data_df["rating"].append(line[2])
+    data_df["timestamp"].append(line[3])
+
+start_time = time()
+df_pd = pd.DataFrame.from_dict(data_df)
+end_time = time()
+print("Time to create Pandas  Dataframe in seconds:", end_time - start_time)
+start_time = time()
+df_pl = pl.from_dict(data_df)
+end_time = time()
+print("Time to create Polars  Dataframe in seconds:", end_time - start_time)
+
+start_time = time()
+df_pd_count = df_pd.groupby("rating")["rating"].count()
+end_time = time()
+print(
+    "Time to create Pandas Dataframe count in seconds:", end_time - start_time
+)
+print(df_pd_count.head())
+start_time = time()
+df_pl_count = df_pl.groupby("rating", maintain_order=True).count()
+end_time = time()
+print(
+    "Time to create Polars Dataframe count in seconds:", end_time - start_time
+)
+print(df_pl_count.head())
--- spark-apps/u_data.py
+++ spark-apps/u_data.py
@@ -1,13 +1,36 @@
 from pyspark import SparkConf, SparkContext
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import countDistinct
+
 import collections
 
-conf = SparkConf().setMaster("local[*]").setAppName("MovieRatings")
+
+def print_line_rdd(line):
+    print(line, len(line))
+
+
+conf = SparkConf().setMaster("spark://spark:7077").setAppName("MovieRatings")
+# conf = SparkConf().setMaster("local").setAppName("MovieRatings")
 sc = SparkContext(conf=conf)
+spark = SparkSession(sc)
+spark.sparkContext.setLogLevel("WARN")
 
-lines = sc.textFile("file:///opt/spark-data/ml-100k/u.data")
-ratings = lines.map(lambda x: x.split()[2])
-result = ratings.countByValue()
+lines_rdd = sc.textFile("file:///opt/spark-data/ml-100k/u.data")
+print("Partitions:", lines_rdd.getNumPartitions())
+# Getting a dataframe from a RDD.
+lines_rdd.foreach(print_line_rdd)
+lines_rdd = lines_rdd.map(lambda line: line.split())
+lines_rdd.foreach(print_line_rdd)
+lines_df = lines_rdd.toDF(["client_id", "movie_id", "rating", "timestamp"])
+lines_df.show()
 
+# Simple operations on a rdd
+ratings = lines_rdd.map(lambda x: x[2])
+result_rdd = ratings.countByValue()
 sortedResults = collections.OrderedDict(sorted(result.items()))
 for k, v in sortedResults.items():
     print(k, v)
+
+# Simple operations on a DataFrame
+result_df = lines_df.groupBy("rating").count().sort("rating")
+result.show()
